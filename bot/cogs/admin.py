58772f5c61b8d4748c9fcec76f86f7b9b60387c3
jgo <gojoshua98@gmail.com>
2020-06-22 14:56:23 -0700

    moved new channel creation to moderation cog

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -9,19 +9,6 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='new-channel')
-    @commands.has_role('admin')
-    async def create_channel(self, ctx, channel_name='text channel'):
-        '''
-        Creates new text channel.
-        Usage: -new-channel [name (optional)]
-        '''
-        guild = ctx.guild
-        existing_channel = discord.utils.get(guild.channels, name=channel_name)
-        if not existing_channel:
-            print(f'Creating a new channel: {channel_name}')
-            await guild.create_text_channel(channel_name)
-
 
 def setup(bot):
     bot.add_cog(Admin(bot))
