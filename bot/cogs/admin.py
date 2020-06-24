c2c3cbfef72dfacaf47a764a0014ed018b8386e9
jgo <gojoshua98@gmail.com>
2020-06-24 15:55:55 -0700

    moved parts of mute and unmute commands and moved them to admin cog

--- bot/cogs/admin.py
+++ bot/cogs/admin.py
@@ -9,6 +9,38 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.has_permissions(administrator=True)
+    @commands.command(name="setup")
+    async def first_time_setup(self, ctx):
+        '''
+        This command must be used for best functionality of the bot. Failure to
+        do so will result in bugs.
+        Usage: !setup
+        '''
+        await ctx.send("Setting up server for best possible functionality...")
+        print("Performing setup for the server...")
+        # Setup the Muted role for mute and unmute commands
+        print("Setting up Muted role...")
+        role_name = "Muted"
+        if not discord.utils.get(ctx.guild.roles, name=role_name):
+            # Muted role does not exist, create it
+            perm = discord.Permissions(    # defaults to no permissions allowed
+                read_message_history=True
+            )
+            await ctx.guild.create_role(
+                name=role_name,
+                permissions=perm,
+                reason="There was no 'Muted' role previously to mute members."
+            )
+            print("Created Muted role...")
+        role = discord.utils.get(ctx.guild.roles, name=role_name)
+        # Set permissions for the muted role in each text channel
+        print("Setting text channel permissions for Muted role...")
+        for channel in ctx.guild.text_channels:
+            await channel.set_permissions(role, send_messages=False)
+        await ctx.send("Setup is now complete.")
+        print("Setup is complete.")
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
