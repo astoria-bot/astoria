1f7fae75affe9c5e1c889f22f5d7308fc56eae16
jgo <gojoshua98@gmail.com>
2020-06-29 14:11:57 -0700

    added logging

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,10 @@
 import os
+import logging
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-status = "!help | suffering"      # Set bot status message here
+status = "!help"      # Set bot status message here
 
 load_dotenv()
 
@@ -16,6 +17,22 @@ MAIN_CHANNEL = int(os.getenv('CHANNEL_ID'))
 
 bot = commands.Bot(command_prefix='!')
 
+# Log errors and debug information
+
+# Simple logger setup, outputs to console
+logging.basicConfig(level=logging.INFO)     
+
+# More advanced logger setup
+# logger = logging.getLogger('discord')
+# logger.setLevel(logging.DEBUG)
+# handler = logging.FileHandler(
+#     filename='discord.log', encoding='utf-8', mode='w'
+# )
+# handler.setFormatter(logging.Formatter(
+#     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
+#     )
+# )
+# logger.addHandler(handler)
 
 @bot.event
 async def on_ready():
