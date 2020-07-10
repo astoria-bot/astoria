292607c572c662dbc30425759f9ad5e0996229d0
jgo <gojoshua98@gmail.com>
2020-07-10 12:08:29 -0700

    added cooldown for processing user messages

--- bot/cogs/levels.py
+++ bot/cogs/levels.py
@@ -10,10 +10,33 @@ class Levels(commands.Cog):
         self.metadata = DBConfig().get_metadata()
         self.conn = DBConfig().get_connection()
         self.db = Methods(self.engine, self.metadata, self.conn)
+        # Set the cooldown for processing messages from a user
+        self._cd = commands.CooldownMapping.from_cooldown(
+            1.0, 60.0, commands.BucketType.user)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        '''
+        Add new members to the leveling database.
+        '''
         self.db.add_user(member.id, member.name, 1, 0, 0)
 
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        '''
+        Grants user experience points for a message sent every 60 seconds per
+        user.
+        '''
+        # Cooldown for updating user stats in leveling system
+        bucket = self._cd.get_bucket(msg)
+        retry_after = bucket.update_rate_limit()
+        if retry_after:
+             # User is rate limited
+            pass
+        else:
+            # User is not rate limited
+            msg_sent(msg.author.id, msg.author.name)
+          
+    
 def setup(bot):
     bot.add_cog(Levels(bot))
